757b514093523ae94826a5692bad54667adb623f
renzzzi <renzdavidespiritu2006@gmail.com>
2025-10-14 00:21:57 +0800

    Added all the different pages for the admin page and use the Unified Dashboard Layout.
    - The pages for the admin page are: Dashboard, Inventory, Purchase Orders, Requests, Settings, Users
    
    - index.php now uses partials, header.php, and footer.php.
    
    - When pressing the tabs at the navigation bar, the main content loads the requested page.

--- admin/index.php
+++ admin/index.php
@@ -1,34 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Office Supply | Admin</title>
-    <link rel="stylesheet" href="../assets/universal-style.css">
-    <link rel="stylesheet" href="../assets/admin-dashboard.css">
-</head>
-<body>
-    <header>
-        <nav class="navbar">
-            <ul>
-                <li><a href="">Dashboard</a></li>
-                <li><a href="">Requests</a></li>
-                <li><a href="">Inventory</a></li>
-                <li><a href="">Users</a></li>
-                <li><a href="">Reports</a></li>
-                <li><a href="">Settings</a></li>
-                <li><a href="">Logout</a></li>
-            </ul>
-        </nav>
-    </header>
-    <main>
-        <div id="content-section">
+<?php
 
-        </div>
-    </main>
-    <footer>
+// Header
+require_once "partials/header.php";
 
-    </footer>
+// Main
+$page = $_GET["page"] ?? "dashboard";
+$pageToShow = "pages/" . $page . ".php";
+require_once $pageToShow;
 
-</body>
-</html>
+// Footer
+require_once "partials/footer.php";
+?>
---
+++ admin/partials/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Office Supply | Admin</title>
+    <link rel="stylesheet" href="../assets/universal-style.css">
+    <link rel="stylesheet" href="../assets/admin-dashboard.css">
+</head>
+<body>
+    <header>
+        <nav class="navbar">
+            <ul>
+                <li><a href="index.php?page=dashboard">Dashboard</a></li>
+                <li><a href="index.php?page=inventory">Inventory</a></li>
+                <li><a href="index.php?page=requests">Requests</a></li>
+                <li><a href="index.php?page=purchase-orders">Purchase Orders</a></li>
+                <li><a href="index.php?page=users">Users</a></li>
+                <li><a href="index.php?page=settings">Settings</a></li>
+                <li><a href="">Switch Account</a></li>
+                <li><a href="">Logout</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
